1c63e01e7066f06715d0d90e9d377638e02e7ae9
Gia Cuong <legiacuong789@gmail.com>
2025-05-20 06:53:28 +0700

    merge branch trung

--- client/src/Pages/OwnerPages/orderOwner/index.jsx
+++ client/src/Pages/OwnerPages/orderOwner/index.jsx
@@ -14,297 +14,301 @@ import { useOrder } from "../../../Contexts/Order/index.jsx";
 import OrderService from "../../../Services/order/index.js";
 
 const FormControl = React.lazy(() =>
-  import("../../../Components/FormControl/index.jsx")
+	import("../../../Components/FormControl/index.jsx")
 );
-const Loading = React.lazy(() => import("../../../Components/Loading/index.jsx"));
+const Loading = React.lazy(() =>
+	import("../../../Components/Loading/index.jsx")
+);
 const Form = React.lazy(() => import("./Form/index.jsx"));
 
 const SubHeader = ({ selectedRows, handleDeleteMultiple }) => {
-  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+	const [showConfirmDelete, setShowConfirmDelete] = useState(false);
 
-  if (selectedRows.length === 0) {
-    return null;
-  }
+	if (selectedRows.length === 0) {
+		return null;
+	}
 
-  return (
-    <>
-      <div className='w-full p-5 bg-gray-100 flex items-center justify-between'>
-        <span>{selectedRows.length} selected rows</span>
-        <button onClick={() => setShowConfirmDelete(true)}>
-          <IoTrashOutline
-            size={36}
-            className='cursor-pointer rounded-full bg-red-100 hover:bg-red-200 text-red-500 p-2'
-          />
-        </button>
-      </div>
+	return (
+		<>
+			<div className='w-full p-5 bg-gray-100 flex items-center justify-between'>
+				<span>{selectedRows.length} selected rows</span>
+				<button onClick={() => setShowConfirmDelete(true)}>
+					<IoTrashOutline
+						size={36}
+						className='cursor-pointer rounded-full bg-red-100 hover:bg-red-200 text-red-500 p-2'
+					/>
+				</button>
+			</div>
 
-      <ConfirmPopup
-        toggle={showConfirmDelete}
-        setToggle={() => setShowConfirmDelete(false)}
-        onOk={handleDeleteMultiple}
-        onCancel={() => setShowConfirmDelete(false)}
-        title='Are you sure you want to delete this?'
-        message='This action can be undone'
-        okButtonText='OK'
-        cancelButtonText='Cancel'
-      />
-    </>
-  );
+			<ConfirmPopup
+				toggle={showConfirmDelete}
+				setToggle={() => setShowConfirmDelete(false)}
+				onOk={handleDeleteMultiple}
+				onCancel={() => setShowConfirmDelete(false)}
+				title='Are you sure you want to delete this?'
+				message='This action can be undone'
+				okButtonText='OK'
+				cancelButtonText='Cancel'
+			/>
+		</>
+	);
 };
 
 const SelectBox = React.forwardRef(({ ...props }) => {
-  return (
-    <input
-      type='checkbox'
-      {...props}
-      className='w-4 h-4 text-[#435d63] bg-[#435d63] border-gray-200 rounded focus:ring-[#435d63]'
-    />
-  );
+	return (
+		<input
+			type='checkbox'
+			{...props}
+			className='w-4 h-4 text-[#435d63] bg-[#435d63] border-gray-200 rounded focus:ring-[#435d63]'
+		/>
+	);
 });
 
 const OrderOwner = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const [errors, setErrors] = useState("");
-  const [showActions, setShowActions] = useState(false);
-  const [selectedRows, setSelectedRows] = useState([]);
-  const [showForm, setShowForm] = useState(false);
-  const {currentStoreOrders, fetchOrdersByCurrentStore, loading} = useOrder();
-  const [editOrder, setEditOrder] = useState(null);
-  const [selectedRow, setSelectedRow] = useState(null);
+	const [searchInput, setSearchInput] = useState("");
+	const [errors, setErrors] = useState("");
+	const [showActions, setShowActions] = useState(false);
+	const [selectedRows, setSelectedRows] = useState([]);
+	const [showForm, setShowForm] = useState(false);
+	const { currentStoreOrders, fetchOrdersByCurrentStore, loading } = useOrder();
+	const [editOrder, setEditOrder] = useState(null);
+	const [selectedRow, setSelectedRow] = useState(null);
 
-  useEffect(() => {
-    const initData = async()=> {
-      console.log("Current store orders:", currentStoreOrders);
-      await fetchOrdersByCurrentStore();
-    }
-    initData();
-  }, []);
+	useEffect(() => {
+		const initData = async () => {
+			console.log("Current store orders:", currentStoreOrders);
+			await fetchOrdersByCurrentStore();
+		};
+		initData();
+	}, []);
 
-  const columns = [
-    {
-      name: "Order Time",
-      sortable: true,
-      selector: (row) => row.created_at,
-    },
-    {
-      name: "Customer",
-      sortable: true,
-      selector: (row) => row.user.name,
-    },
-    {
-      name: "Shipping Address",
-      sortable: true,
-      selector: (row) => row.shipping_address,
-    },
-    {
-      name: "Total Price",
-      sortable: true,
-      selector: (row) => row.total_amount,
-    },
-    {
-      name: "Status",
-      sortable: true,
-      selector: (row) => row.status,
-    },
-    {
-      name: "Actions",
-      center: true,
-      cell: (row) => (
-        <div className='flex gap-2'>
-          <FaRegEdit
-            className={`cursor-pointer ${row.status === ("Completed"||"Canceled") ? "text-gray-400 pointer-events-none" : "text-black"}`}
-            size={18}
-            onClick={() => handleEdit(row)}
-          />
-        </div>
-      ),
-      ignoreRowClick: true,
-    },
-  ];
+	const columns = [
+		{
+			name: "Order Time",
+			sortable: true,
+			selector: (row) => row.created_at,
+		},
+		{
+			name: "Customer",
+			sortable: true,
+			selector: (row) => row.user.name,
+		},
+		{
+			name: "Shipping Address",
+			sortable: true,
+			selector: (row) => row.shipping_address,
+		},
+		{
+			name: "Total Price",
+			sortable: true,
+			selector: (row) => row.total_amount,
+		},
+		{
+			name: "Status",
+			sortable: true,
+			selector: (row) => row.status,
+		},
+		{
+			name: "Actions",
+			center: true,
+			cell: (row) => (
+				<div className='flex gap-2'>
+					<FaRegEdit
+						className={`cursor-pointer ${
+							row.status === ("Completed" || "Canceled")
+								? "text-gray-400 pointer-events-none"
+								: "text-black"
+						}`}
+						size={18}
+						onClick={() => handleEdit(row)}
+					/>
+				</div>
+			),
+			ignoreRowClick: true,
+		},
+	];
 
-  const handleFieldsChange = (key, value) => {
-    setSearchInput(value);
-  };
+	const handleFieldsChange = (key, value) => {
+		setSearchInput(value);
+	};
 
-  const handleRowsSelected = ({ selectedRows }) => {
-    setSelectedRows(selectedRows);
-  };
+	const handleRowsSelected = ({ selectedRows }) => {
+		setSelectedRows(selectedRows);
+	};
 
-  const handleCancelMultiple = async () => {
-    const ids = selectedRows.map((row) => row.id);
-    try {
-      await UserService.deleteMultipleUsers(ids);
-      //setUsersData(usersData.filter((user) => !ids.includes(user.id)));
-      setSelectedRows([]);
-      showToast("Xóa nhiều thành công", "success");
-    } catch (error) {
-      console.error("Lỗi khi xóa nhiều:", error);
-      showToast("Lỗi khi xóa nhiều", "error");
-    }
-  };
+	const handleCancelMultiple = async () => {
+		const ids = selectedRows.map((row) => row.id);
+		try {
+			await UserService.deleteMultipleUsers(ids);
+			//setUsersData(usersData.filter((user) => !ids.includes(user.id)));
+			setSelectedRows([]);
+			showToast("Xóa nhiều thành công", "success");
+		} catch (error) {
+			console.error("Lỗi khi xóa nhiều:", error);
+			showToast("Lỗi khi xóa nhiều", "error");
+		}
+	};
 
-  const handleEdit = (order) => {
-    setShowForm(true);
-    setEditOrder(order);
-  };
+	const handleEdit = (order) => {
+		setShowForm(true);
+		setEditOrder(order);
+	};
 
-  const handleFormSubmit = async (newUserData) => {
-    // try {
-    //   if (editUser) {
-    //     const response = await UserService.updateUser(editUser.id, newUserData);
-    //     setUsersData(
-    //       usersData.map((user) =>
-    //         user.id === editUser.id ? response.data : user
-    //       )
-    //     );
-    //     showToast("Cập nhật thành công", "success");
-    //   } else {
-    //     const response = await UserService.createUser(newUserData);
-    //     setUsersData([...usersData, response.data]);
-    //     showToast("Tạo thành công", "success");
-    //   }
-    //   setShowForm(false);
-    //   setEditUser(null);
-    // } catch (error) {
-    //   console.error("Lỗi khi lưu:", error);
-    //   const errorMessage =
-    //     typeof error === "string" ? error : error.message || "Lỗi";
-    //   showToast(`Lỗi khi lưu: ${errorMessage}`, "error");
-    // }
-  };
+	const handleFormSubmit = async (newUserData) => {
+		// try {
+		//   if (editUser) {
+		//     const response = await UserService.updateUser(editUser.id, newUserData);
+		//     setUsersData(
+		//       usersData.map((user) =>
+		//         user.id === editUser.id ? response.data : user
+		//       )
+		//     );
+		//     showToast("Cập nhật thành công", "success");
+		//   } else {
+		//     const response = await UserService.createUser(newUserData);
+		//     setUsersData([...usersData, response.data]);
+		//     showToast("Tạo thành công", "success");
+		//   }
+		//   setShowForm(false);
+		//   setEditUser(null);
+		// } catch (error) {
+		//   console.error("Lỗi khi lưu:", error);
+		//   const errorMessage =
+		//     typeof error === "string" ? error : error.message || "Lỗi";
+		//   showToast(`Lỗi khi lưu: ${errorMessage}`, "error");
+		// }
+	};
 
-  const handleRowClicked = (row) => {
-    setSelectedRow(row);
-  };
+	const handleRowClicked = (row) => {
+		setSelectedRow(row);
+	};
 
-  const handleActionsClicked = () => {
-    setShowActions(!showActions);
-  };
+	const handleActionsClicked = () => {
+		setShowActions(!showActions);
+	};
 
-  const handleImport = () => {
-    alert("Developing.");
-  };
+	const handleImport = () => {
+		alert("Developing.");
+	};
 
-  const handleExport = () => {
-    alert("Developing.");
-  };
+	const handleExport = () => {
+		alert("Developing.");
+	};
 
-  // const filteredData = usersData.filter((user) =>
-  //   user.name.toLowerCase().includes(searchInput.toLowerCase())
-  // );
+	// const filteredData = usersData.filter((user) =>
+	//   user.name.toLowerCase().includes(searchInput.toLowerCase())
+	// );
 
-  return (
-    <>
-      <section>
-        <div>
-          <header className='bg-white rounded-md p-4 flex items-center justify-between shadow-md'>
-            <div className='min-w-fit max-w-md'>
-              <FormControl
-                type='text'
-                placeHolder='Search here...'
-                wrapInputStyle='!border-black/10 rounded-md focus-within:!border-[#435d63] transition-all'
-                inputStyle='font-serif placeholder:text-lg text-black placeholder:font-serif !p-4 !py-2'
-                id='search'
-                onChange={(event) =>
-                  handleFieldsChange("search", event.target.value)
-                }
-                hasButton
-                Icon={CiSearch}
-                iconSize={24}
-                iconStyle='transition-all text-[#435d63] hover:text-black mx-4'
-                hasError={errors?.searchInput}
-                errorMessage={errors?.searchInput}
-              />
-            </div>
+	return (
+		<>
+			<section>
+				<div>
+					<header className='bg-white rounded-md p-4 flex items-center justify-between shadow-md'>
+						<div className='min-w-fit max-w-md'>
+							<FormControl
+								type='text'
+								placeHolder='Search here...'
+								wrapInputStyle='!border-black/10 rounded-md focus-within:!border-[#435d63] transition-all'
+								inputStyle='font-serif placeholder:text-lg text-black placeholder:font-serif !p-4 !py-2'
+								id='search'
+								onChange={(event) =>
+									handleFieldsChange("search", event.target.value)
+								}
+								hasButton
+								Icon={CiSearch}
+								iconSize={24}
+								iconStyle='transition-all text-[#435d63] hover:text-black mx-4'
+								hasError={errors?.searchInput}
+								errorMessage={errors?.searchInput}
+							/>
+						</div>
 
-            <div className='relative'>
-              <button
-                type='button'
-                className='text-sm rounded-md w-fit transition-all duration-700 hover:bg-black text-white bg-[#435d63] p-2 font-serif font-semibold'
-                onClick={handleActionsClicked}>
-                <p>Action</p>
-              </button>
-              {showActions && (
-                <div className='overflow-hidden absolute z-10 top-full right-0 rounded-md bg-white w-fit shadow-md'>
-                  <button
-                    className='p-2 px-4 hover:bg-black/10 w-full'
-                    onClick={handleExport}>
-                    Export
-                  </button>
-                  <button
-                    className='p-2 px-4 hover:bg-black/10 w-full'
-                    onClick={handleImport}>
-                    Import
-                  </button>
-                </div>
-              )}
-            </div>
-          </header>
+						<div className='relative'>
+							<button
+								type='button'
+								className='text-sm rounded-md w-fit transition-all duration-700 hover:bg-black text-white bg-[#435d63] p-2 font-serif font-semibold'
+								onClick={handleActionsClicked}>
+								<p>Action</p>
+							</button>
+							{showActions && (
+								<div className='overflow-hidden absolute z-10 top-full right-0 rounded-md bg-white w-fit shadow-md'>
+									<button
+										className='p-2 px-4 hover:bg-black/10 w-full'
+										onClick={handleExport}>
+										Export
+									</button>
+									<button
+										className='p-2 px-4 hover:bg-black/10 w-full'
+										onClick={handleImport}>
+										Import
+									</button>
+								</div>
+							)}
+						</div>
+					</header>
 
-          <main className='mt-4 rounded-md shadow-md overflow-hidden'>
-            {loading ? (
-              <Loading />
-            ) : (
-              <DataTable
-                customStyles={{
-                  subHeader: {
-                    style: { padding: "0", margin: "0", minHeight: "0" },
-                  },
-                }}
-                pointerOnHover
-                highlightOnHover
-                selectableRows
-                striped
-                pagination
-                onSelectedRowsChange={handleRowsSelected}
-                subHeader={selectedRows.length > 0}
-                subHeaderComponent={
-                  <SubHeader
-                    selectedRows={selectedRows}
-                    handleDeleteMultiple={handleCancelMultiple}
-                  />
-                }
-                columns={columns}
-                data={currentStoreOrders}
-                selectableRowsComponent={SelectBox}
-                selectableRowsComponentProps={{
-                  style: {
-                    backgroundColor: "white",
-                    borderColor: "#435d63",
-                    accentColor: "#435d63",
-                  },
-                }}
-                onRowClicked={handleRowClicked}
-              />
-            )}
-          </main>
-        </div>
-      </section>
+					<main className='mt-4 rounded-md shadow-md overflow-hidden'>
+						{loading ? (
+							<Loading />
+						) : (
+							<DataTable
+								customStyles={{
+									subHeader: {
+										style: { padding: "0", margin: "0", minHeight: "0" },
+									},
+								}}
+								pointerOnHover
+								highlightOnHover
+								selectableRows
+								striped
+								pagination
+								onSelectedRowsChange={handleRowsSelected}
+								subHeader={selectedRows.length > 0}
+								subHeaderComponent={
+									<SubHeader
+										selectedRows={selectedRows}
+										handleDeleteMultiple={handleCancelMultiple}
+									/>
+								}
+								columns={columns}
+								data={currentStoreOrders}
+								selectableRowsComponent={SelectBox}
+								selectableRowsComponentProps={{
+									style: {
+										backgroundColor: "white",
+										borderColor: "#435d63",
+										accentColor: "#435d63",
+									},
+								}}
+								onRowClicked={handleRowClicked}
+							/>
+						)}
+					</main>
+				</div>
+			</section>
 
-      <Form
-        toggle={showForm}
-        setToggle={() => {
-          setShowForm(false);
-          setEditOrder(null);
-        }}
-        initialData={editOrder}
-        onSubmit={handleFormSubmit}
-        isDisabled={false}
-        fetchCurrentStoreOrders={fetchOrdersByCurrentStore}
-      />
+			<Form
+				toggle={showForm}
+				setToggle={() => {
+					setShowForm(false);
+					setEditOrder(null);
+				}}
+				initialData={editOrder}
+				onSubmit={handleFormSubmit}
+				isDisabled={false}
+				fetchCurrentStoreOrders={fetchOrdersByCurrentStore}
+			/>
 
-      <Form
-        toggle={!!selectedRow}
-        setToggle={() => setSelectedRow(null)}
-        initialData={selectedRow}
-        onSubmit={() => {}}
-        isDisabled={true}
-        check={"hi"}
-      />
-
-      <ToastContainer />
-    </>
-  );
+			<Form
+				toggle={!!selectedRow}
+				setToggle={() => setSelectedRow(null)}
+				initialData={selectedRow}
+				onSubmit={() => {}}
+				isDisabled={true}
+				check={"hi"}
+			/>
+		</>
+	);
 };
 
 export default OrderOwner;
--- client/src/Pages/OwnerPages/paymentOwner/index.jsx
+++ client/src/Pages/OwnerPages/paymentOwner/index.jsx
@@ -14,160 +14,165 @@ import { usePayment } from "../../../Contexts/Payment/index.jsx";
 import PaymentService from "../../../Services/payment/index.jsx";
 
 const FormControl = React.lazy(() =>
-  import("../../../Components/FormControl/index.jsx")
+	import("../../../Components/FormControl/index.jsx")
 );
-const Loading = React.lazy(() => import("../../../Components/Loading/index.jsx"));
+const Loading = React.lazy(() =>
+	import("../../../Components/Loading/index.jsx")
+);
 
 const PaymentOwner = () => {
-  const [searchInput, setSearchInput] = useState("");
-  const [errors, setErrors] = useState("");
-  const [showActions, setShowActions] = useState(false);
-  const [selectedRows, setSelectedRows] = useState([]);
-  const [showForm, setShowForm] = useState(false);
-  const {currentStorePayments, fetchPaymentsByCurrentStore, loading} = usePayment();
-  const [editOrder, setEditOrder] = useState(null);
-  const [selectedRow, setSelectedRow] = useState(null);
+	const [searchInput, setSearchInput] = useState("");
+	const [errors, setErrors] = useState("");
+	const [showActions, setShowActions] = useState(false);
+	const [selectedRows, setSelectedRows] = useState([]);
+	const [showForm, setShowForm] = useState(false);
+	const { currentStorePayments, fetchPaymentsByCurrentStore, loading } =
+		usePayment();
+	const [editOrder, setEditOrder] = useState(null);
+	const [selectedRow, setSelectedRow] = useState(null);
 
-  useEffect(() => {
-    const initData = async()=> {
-      console.log("Current store payments:", currentStorePayments);
-      await fetchPaymentsByCurrentStore();
-    }
-    initData();
-  }, []);
+	useEffect(() => {
+		const initData = async () => {
+			console.log("Current store payments:", currentStorePayments);
+			await fetchPaymentsByCurrentStore();
+		};
+		initData();
+	}, []);
 
-  const columns = [
-    {
-      name: "Payment Time",
-      sortable: true,
-      selector: (row) => row.payment_date,
-    },
-    {
-      name: "Appointment",
-      sortable: true,
-      selector: (row) => row.appointment.id,
-    },
-    {
-      name: "Total Price",
-      sortable: true,
-      selector: (row) => row.price,
-    },
-    {
-      name: "Actions",
-      center: true,
-      cell: (row) => (
-        <div className='flex gap-2'>
-          <FaRegEdit
-            className={`cursor-pointer ${row.status === ("Completed"||"Canceled") ? "text-gray-400 pointer-events-none" : "text-black"}`}
-            size={18}
-            onClick={() => {}}
-          />
-        </div>
-      ),
-      ignoreRowClick: true,
-    },
-  ];
+	const columns = [
+		{
+			name: "Payment Time",
+			sortable: true,
+			selector: (row) => row.payment_date,
+		},
+		{
+			name: "Appointment",
+			sortable: true,
+			selector: (row) => row.appointment.id,
+		},
+		{
+			name: "Total Price",
+			sortable: true,
+			selector: (row) => row.price,
+		},
+		{
+			name: "Actions",
+			center: true,
+			cell: (row) => (
+				<div className='flex gap-2'>
+					<FaRegEdit
+						className={`cursor-pointer ${
+							row.status === ("Completed" || "Canceled")
+								? "text-gray-400 pointer-events-none"
+								: "text-black"
+						}`}
+						size={18}
+						onClick={() => {}}
+					/>
+				</div>
+			),
+			ignoreRowClick: true,
+		},
+	];
 
-  const handleFieldsChange = (key, value) => {
-    setSearchInput(value);
-  };
+	const handleFieldsChange = (key, value) => {
+		setSearchInput(value);
+	};
 
-  const handleRowClicked = (row) => {
-    setSelectedRow(row);
-  };
+	const handleRowClicked = (row) => {
+		setSelectedRow(row);
+	};
 
-  const handleActionsClicked = () => {
-    setShowActions(!showActions);
-  };
+	const handleActionsClicked = () => {
+		setShowActions(!showActions);
+	};
 
-  const handleImport = () => {
-    alert("Developing.");
-  };
+	const handleImport = () => {
+		alert("Developing.");
+	};
 
-  const handleExport = () => {
-    alert("Developing.");
-  };
+	const handleExport = () => {
+		alert("Developing.");
+	};
 
-  // const filteredData = usersData.filter((user) =>
-  //   user.name.toLowerCase().includes(searchInput.toLowerCase())
-  // );
+	// const filteredData = usersData.filter((user) =>
+	//   user.name.toLowerCase().includes(searchInput.toLowerCase())
+	// );
 
-  return (
-    <>
-      <section>
-        <div>
-          <header className='bg-white rounded-md p-4 flex items-center justify-between shadow-md'>
-            <div className='min-w-fit max-w-md'>
-              <FormControl
-                type='text'
-                placeHolder='Search here...'
-                wrapInputStyle='!border-black/10 rounded-md focus-within:!border-[#435d63] transition-all'
-                inputStyle='font-serif placeholder:text-lg text-black placeholder:font-serif !p-4 !py-2'
-                id='search'
-                onChange={(event) =>
-                  handleFieldsChange("search", event.target.value)
-                }
-                hasButton
-                Icon={CiSearch}
-                iconSize={24}
-                iconStyle='transition-all text-[#435d63] hover:text-black mx-4'
-                hasError={errors?.searchInput}
-                errorMessage={errors?.searchInput}
-              />
-            </div>
+	return (
+		<>
+			<section>
+				<div>
+					<header className='bg-white rounded-md p-4 flex items-center justify-between shadow-md'>
+						<div className='min-w-fit max-w-md'>
+							<FormControl
+								type='text'
+								placeHolder='Search here...'
+								wrapInputStyle='!border-black/10 rounded-md focus-within:!border-[#435d63] transition-all'
+								inputStyle='font-serif placeholder:text-lg text-black placeholder:font-serif !p-4 !py-2'
+								id='search'
+								onChange={(event) =>
+									handleFieldsChange("search", event.target.value)
+								}
+								hasButton
+								Icon={CiSearch}
+								iconSize={24}
+								iconStyle='transition-all text-[#435d63] hover:text-black mx-4'
+								hasError={errors?.searchInput}
+								errorMessage={errors?.searchInput}
+							/>
+						</div>
 
-            <div className='relative'>
-              <button
-                type='button'
-                className='text-sm rounded-md w-fit transition-all duration-700 hover:bg-black text-white bg-[#435d63] p-2 font-serif font-semibold'
-                onClick={handleActionsClicked}>
-                <p>Action</p>
-              </button>
-              {showActions && (
-                <div className='overflow-hidden absolute z-10 top-full right-0 rounded-md bg-white w-fit shadow-md'>
-                  <button
-                    className='p-2 px-4 hover:bg-black/10 w-full'
-                    onClick={handleExport}>
-                    Export
-                  </button>
-                  <button
-                    className='p-2 px-4 hover:bg-black/10 w-full'
-                    onClick={handleImport}>
-                    Import
-                  </button>
-                </div>
-              )}
-            </div>
-          </header>
+						<div className='relative'>
+							<button
+								type='button'
+								className='text-sm rounded-md w-fit transition-all duration-700 hover:bg-black text-white bg-[#435d63] p-2 font-serif font-semibold'
+								onClick={handleActionsClicked}>
+								<p>Action</p>
+							</button>
+							{showActions && (
+								<div className='overflow-hidden absolute z-10 top-full right-0 rounded-md bg-white w-fit shadow-md'>
+									<button
+										className='p-2 px-4 hover:bg-black/10 w-full'
+										onClick={handleExport}>
+										Export
+									</button>
+									<button
+										className='p-2 px-4 hover:bg-black/10 w-full'
+										onClick={handleImport}>
+										Import
+									</button>
+								</div>
+							)}
+						</div>
+					</header>
 
-          <main className='mt-4 rounded-md shadow-md overflow-hidden'>
-            {loading ? (
-              <Loading />
-            ) : (
-              <DataTable
-                customStyles={{
-                  subHeader: {
-                    style: { padding: "0", margin: "0", minHeight: "0" },
-                  },
-                }}
-                pointerOnHover
-                highlightOnHover
-                selectableRows
-                striped
-                pagination
-                subHeader={selectedRows.length > 0}
-                columns={columns}
-                data={currentStorePayments}
-                onRowClicked={handleRowClicked}
-              />
-            )}
-          </main>
-        </div>
-      </section>
-
-      <ToastContainer />
-    </>
-  );
+					<main className='mt-4 rounded-md shadow-md overflow-hidden'>
+						{loading ? (
+							<Loading />
+						) : (
+							<DataTable
+								customStyles={{
+									subHeader: {
+										style: { padding: "0", margin: "0", minHeight: "0" },
+									},
+								}}
+								pointerOnHover
+								highlightOnHover
+								selectableRows
+								striped
+								pagination
+								subHeader={selectedRows.length > 0}
+								columns={columns}
+								data={currentStorePayments}
+								onRowClicked={handleRowClicked}
+							/>
+						)}
+					</main>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default PaymentOwner;
